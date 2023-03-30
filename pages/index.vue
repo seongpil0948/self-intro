c87179ff2ac80a95404c7d43e77f5ca9fc7eb186
seongpil0948 <seongpil0948@gmail.com>
2023-03-30 12:22:57 +0900

    add three js

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>home page</div>
+  <n-space vertical>
+    <div>home page</div>
+    <canvas ref="experience" />
+  </n-space>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Mesh,
+  SphereGeometry,
+  MeshBasicMaterial,
+} from "three";
+import { NSpace } from "naive-ui";
+const experience = ref<HTMLCanvasElement | null>(null);
+
+onMounted(() => {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.devicePixelRatio, // window.innerWidth / window.innerHeight
+    0.1,
+    1000
+  );
+  scene.add(camera);
+
+  const sphere = new Mesh(
+    new SphereGeometry(1, 20, 20),
+    new MeshBasicMaterial({ color: 0x00ff00 })
+  );
+  scene.add(sphere);
+  camera.position.z = 5;
+  const renderer = new WebGLRenderer({
+    canvas: experience.value as HTMLCanvasElement,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
+});
+</script>

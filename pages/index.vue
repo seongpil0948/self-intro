323148aa6a7642144536b6ebfc162816986c64d2
seongpil0948 <seongpil0948@gmail.com>
2023-03-31 17:02:01 +0900

    delete three

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,5 @@
 <template>
   <v-container>
     <div>home page</div>
-    <canvas ref="canvasEl" />
   </v-container>
 </template>
-
-<script setup lang="ts">
-import {
-  Scene,
-  PerspectiveCamera,
-  WebGLRenderer,
-  Mesh,
-  SphereGeometry,
-  MeshBasicMaterial,
-} from "three";
-const canvasEl = ref<HTMLCanvasElement | null>(null);
-
-onMounted(() => {
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(
-    75,
-    window.devicePixelRatio, // window.innerWidth / window.innerHeight
-    0.1,
-    1000
-  );
-  scene.add(camera);
-
-  const sphere = new Mesh(
-    new SphereGeometry(1, 20, 20),
-    new MeshBasicMaterial({ color: 0x00ff00 })
-  );
-  scene.add(sphere);
-  camera.position.z = 5;
-  const renderer = new WebGLRenderer({
-    canvas: canvasEl.value as HTMLCanvasElement,
-    antialias: true,
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  renderer.render(scene, camera);
-});
-</script>
